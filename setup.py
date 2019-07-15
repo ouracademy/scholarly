61d4ba90f4c01cd43a07c276288784da08c85570
qpdian <qpdiam@gmail.com>
2019-07-14 21:41:33 -0500

    chore: change version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name = 'scholarly',
-    version = '0.2.5',
+    version = '0.2.6',
     author = 'Steven A. Cholewiak',
     author_email = 'steven@cholewiak.com',
 
