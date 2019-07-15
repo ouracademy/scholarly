7cf5c83a88d286bbbe2d67b3f268847468665421
qpdian <qpdiam@gmail.com>
2019-07-14 21:37:36 -0500

    fix: no import extractor

--- scholarly/__init__.py
+++ scholarly/__init__.py
@@ -1,4 +1,4 @@
 from .scholarly import Author, Publication
 from .scholarly import search_author, search_keyword, search_pubs_query
 from .scholarly import search_pubs_custom_url, search_author_custom_url
-from .extractor import fields_by_publication
+
